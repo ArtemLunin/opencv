89fd9d237764a395cd9fbabca4add75c8fc1ac05
ArtemLunin <lunin.mail@gmail.com>
2023-03-05 21:32:36 +0200

    uploads files

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import hashlib
 from flask import Flask, flash, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError, OperationalError, IntegrityError
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = "./uploads"
+UPLOAD_FOLDER = "./static/uploads"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
@@ -27,6 +28,12 @@ def digest_filename(filename):
     [name, ext] = os.path.splitext(base)
     return hashlib.md5(name.encode()).hexdigest() + ext
 
+def remove_files(dir_name):
+    for file in os.scandir(dir_name):
+        if os.path.isfile(file.path) or os.path.islink(file.path):
+            os.remove(file.path)
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     fileId = db.Column(db.String(length=32), nullable=False, unique=True)
@@ -41,7 +48,7 @@ def index():
             flash('No file part')
             return redirect(request.url)
         
-        fileId = request.form['file_id']
+        fileId = request.form['fileId']
         file = request.files['file']
         if file.filename == '':
             flash('No selected file')
@@ -50,20 +57,49 @@ def index():
             secureFilename = secure_filename(file.filename)
             filename = digest_filename(secureFilename)
             item = Item(fileId=fileId, storedName=filename, originalName=secureFilename)
-            db.session.add(item)
-            db.session.commit()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    forms = [
-    {'title': 'form1', 'image': 'img_1.jpg', 'file_id': 'file1'}, 
-    {'title': 'form2', 'image': 'img_2.jpg', 'file_id': 'file2'}
-    ]
-    return render_template('index.html', forms = forms)
+            try:
+                db.session.add(item)
+                db.session.commit()
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            except IntegrityError:
+                # filter_by
+                db.session.rollback()
+                upd_item = db.session.execute(db.select(Item).filter_by(fileId=fileId)).scalar_one_or_none()
+                # print(upd_item.storedName)
+                if upd_item:
+                    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], upd_item.storedName))
+                    upd_item.originalName = secureFilename
+                    upd_item.storedName = filename
+                    db.session.commit()
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # print(upd_item)
+            return redirect(url_for('index'))
+    if request.method == "GET":
+        items = Item.query.all()
+        forms = [
+            {'title': 'form1', 'storedName': 'no_image.png', 'fileId': 'file1'}, 
+            {'title': 'form2', 'storedName': 'no_image.png', 'fileId': 'file2'}
+            ]
+        i = 0
+        for item in items:
+            # print(item.fileId, item.storedName, item.originalName)
+            forms[i]['storedName'] = item.storedName
+            forms[i]['fileId'] = item.fileId
+            i += 1
+    return render_template('index.html', forms=forms)
 
 @app.route("/clear")
 def clear():
-    db.session.query(Item).delete()
-    db.session.commit()
+    try:
+        db.session.query(Item).delete()
+        db.session.commit()
+    except OperationalError as e:
+        db.create_all()
+        db.session.commit()
+    except Exception as err:
+        flash(f"Unexpected {err=}, {type(err)=}")
+    finally:
+        remove_files(app.config['UPLOAD_FOLDER'])
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
